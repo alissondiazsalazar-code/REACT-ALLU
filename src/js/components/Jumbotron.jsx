ba930be4d7b2bbf437869d7f8aa782cf7246c2fc
Luz Alisson Diaz Salazar <alissondiazsalazar@gmail.com>
2026-02-08 17:34:17 +0000

    Add Jumbotron

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+function Jumbotron(props) {
+    return (
+        <div class="p-10 m-5 bg-body-secondary rounded-3">
+            <div class="container-fluid p-5">
+                <h1 class="display-5 ">A Warm Welcome!</h1>
+                <p class="col"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, accusantium optio quos sint deleniti, ad suscipit libero velit culpa enim quam iusto explicabo? Quidem soluta placeat, tempora dolores asperiores inventore!</p>
+                <button class="btn btn-primary btn-lg mt-2 mb-4" type="button">Call to action!</button>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron; 
