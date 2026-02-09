d48f0078a014093947209c854afa135ee39b4e70
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-02-09 11:45:04 +0000

    Card modificacion 2

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,12 +1,14 @@
-function Card(props) {
+import React from "react";
+
+function Card() {
     return (
-        <div class="card-global col-3 d-flex">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card-global col-3 d-flex">
+            <div className="card">
+                <img src="https://placehold.co/500x350" class="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic harum consectetur optio amet explicabo voluptas deleniti maiores nulla dolores eligendi laborum, non, tenetur natus iusto nesciunt ratione nobis eaque enim?</p>
+                        <a href="#" class="btn btn-primary">Find Out More!</a>
                     </div>
             </div>
         </div>
