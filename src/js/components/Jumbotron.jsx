f22ee22bf3db00b9169f7f84c056b28fec9967a7
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-02-09 13:03:27 +0000

    Jumbotron Actualizado 1

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -1,11 +1,9 @@
 function Jumbotron(props) {
     return (
-        <div class="p-10 m-5 bg-body-secondary rounded-3">
-            <div class="container-fluid p-5">
-                <h1 class="display-5 ">A Warm Welcome!</h1>
-                <p class="col"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, accusantium optio quos sint deleniti, ad suscipit libero velit culpa enim quam iusto explicabo? Quidem soluta placeat, tempora dolores asperiores inventore!</p>
-                <button class="btn btn-primary btn-lg mt-2 mb-4" type="button">Call to action!</button>
-            </div>
+        <div class="p-5 m-5 bg-body-secondary rounded-3">
+                <h1 class="display-5 fw-normal ">A Warm Welcome!</h1>
+                <h3 class="col fs-5 my-3 py-2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, accusantium optio quos sint deleniti, ad suscipit libero velit culpa enim quam iusto explicabo? Quidem soluta placeat, tempora dolores asperiores inventore!</h3>
+                <button class="btn btn-primary btn-lg mt-2 mb-3 " type="button">Call to action!</button>
         </div>
     )
 }
