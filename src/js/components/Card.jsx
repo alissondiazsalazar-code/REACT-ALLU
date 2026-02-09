58c707186b92a8ffb6a9306f063e222e940b7446
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-02-09 14:15:29 +0000

    Card modificacion 5 creo

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Card() {
     return (
-        <div className="card-global d-flex">
+        <div className="card-global">
             <div className="card">
-                <img src="https://placehold.co/500x350" class="card-img-top" alt="..."/>
+                <img src="https://placehold.co/500x350" className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title text-center">Card title</h5>
                         <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic harum consectetur optio amet explicabo voluptas deleniti maiores nulla dolores eligendi laborum, non, tenetur natus iusto nesciunt ratione nobis eaque enim?</p>
@@ -12,7 +12,7 @@ function Card() {
                         <div className="row">
                           <div className="col-3"></div>
                           <div className="col-6">
-                            <a href="#" class="btn btn-primary d-flex justify-content-center fw-bold fs-5 text py-2">Find Out More!</a>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center fw-bold fs-5 text py-2">Find Out More!</a>
                           </div>
                           <div className="col-3"></div>
                         </div>
